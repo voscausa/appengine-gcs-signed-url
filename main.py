823211aecc1a962aaf65909b45f52d35b85ad1af
voscausa <voscausa@users.noreply.github.com>
2015-04-25 01:59:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ import webapp2
 routes = [
     webapp2.Route(r'/sign_url', handler='sign_url.SignUrl'),
 ]
-app = webapp2.WSGIApplication(routes=routes, debug=True))
+app = webapp2.WSGIApplication(routes=routes, debug=True)
