21565e8d0295b578a33e9a1d424d6b0c5e143323
voscausa <voscausa@users.noreply.github.com>
2015-04-25 01:17:52 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import webapp2
+
+routes = [
+    webapp2.Route(r'/sign_url', handler='sign_url.SignUrl'),
+]
+app = webapp2.WSGIApplication(routes=routes, debug=True))
