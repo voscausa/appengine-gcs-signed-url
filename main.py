4aced2e0711d303e658b02da772f4f8b1f02a938
voscausa <voscausa@users.noreply.github.com>
2015-04-25 18:41:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ import webapp2
 
 routes = [
     webapp2.Route(r'/download_signed', handler='sign_url.DownloadSigned'),
-    webapp2.Route(r'/sign_url', handler='sign_url.SignUrl'),
+    webapp2.Route(r'/sign_url/<bucket_object:.*>', handler='sign_url.SignUrl'),
 ]
 app = webapp2.WSGIApplication(routes=routes, debug=True)
